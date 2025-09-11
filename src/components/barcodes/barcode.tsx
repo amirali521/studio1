896f90c42a2b8eafd26dbb1e72519f05ea6493f9
Amir Ali <hacktech430@gmail.com>
2025-09-11 14:54:13 +0000

    Try fixing this error: `Runtime Error: Error: qrCodeeUrl is not defined.

--- src/components/barcodes/barcode.tsx
+++ src/components/barcodes/barcode.tsx
@@ -57,7 +57,7 @@ export function BarcodeDisplay({ item, type }: BarcodeDisplayProps) {
         ) : (
             qrCodeUrl ? (
                 <>
-                    <img src={qrCodeeUrl} alt={`QR code for ${serialNumber}`} />
+                    <img src={qrCodeUrl} alt={`QR code for ${serialNumber}`} />
                     <p className="text-xs mt-1 font-mono">{serialNumber}</p>
                 </>
             ) : <p>Generating QR...</p>
