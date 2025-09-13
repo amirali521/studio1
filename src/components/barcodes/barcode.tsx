61767a6dbc8042a6fd76a95597034c6c782db88a
Amir Ali <hacktech430@gmail.com>
2025-09-13 12:32:55 +0000

    Ok you need to create a qr codes in that image type which pixel not burs

--- src/components/barcodes/barcode.tsx
+++ src/components/barcodes/barcode.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 import QRCode from 'qrcode';
 import type { QrCodeData } from '@/lib/types';
 
@@ -12,37 +12,36 @@ interface BarcodeDisplayProps {
 }
 
 export function BarcodeDisplay({ item, size = 150 }: BarcodeDisplayProps) {
-  const [qrCodeUrl, setQrCodeUrl] = useState('');
   const { serialNumber } = item;
 
-  useEffect(() => {
+  const qrCodeSvg = useMemo(() => {
     const qrData = JSON.stringify(item);
-    QRCode.toDataURL(qrData, { 
-      errorCorrectionLevel: 'H', 
-      type: 'image/png',
-      quality: 0.9,
+    let svgString = '';
+    QRCode.toString(qrData, {
+      type: 'svg',
+      errorCorrectionLevel: 'H',
       margin: 2,
-      scale: 8, // Generate a larger image
-    })
-        .then(url => {
-            setQrCodeUrl(url);
-        })
-        .catch(err => {
-            console.error("QR Code generation failed", err);
-        });
+    }, (err, string) => {
+      if (err) {
+          console.error("QR Code SVG generation failed", err);
+          return;
+      };
+      svgString = string;
+    });
+    return svgString;
   }, [item]);
+
 
   return (
     <div 
       className="p-2 border rounded-lg flex flex-col items-center justify-center break-inside-avoid aspect-square"
       style={{ width: `${size}px`, height: `${size}px` }}
     >
-        {qrCodeUrl ? (
+        {qrCodeSvg ? (
             <>
-                <img 
-                  src={qrCodeUrl} 
-                  alt={`QR code for ${serialNumber}`} 
-                  className="w-full h-full object-contain"
+                <div 
+                  className="w-full h-full"
+                  dangerouslySetInnerHTML={{ __html: qrCodeSvg }} 
                 />
                 <p className="text-xs mt-1 font-mono">{serialNumber}</p>
             </>
