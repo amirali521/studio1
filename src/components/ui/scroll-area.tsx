2fb04e07f7199914017ed4975e9fb1361b3584e6
Amir Ali <hacktech430@gmail.com>
2025-09-18 15:43:28 +0000

    The custom scroll bar still available vertical and horizontal

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -18,8 +18,6 @@ const ScrollArea = React.forwardRef<
     <ScrollAreaPrimitive.Viewport ref={viewportRef} className="h-full w-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
-    <ScrollBar />
-    <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ))
 ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName
