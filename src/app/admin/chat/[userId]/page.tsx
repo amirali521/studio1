d78c335bc751f614810e5df72f819f9232b95bb6
Amir Ali <hacktech430@gmail.com>
2025-09-26 06:38:34 +0000

    The admin chatbox need to follow same ui like chat support ui like new c

--- src/app/admin/chat/[userId]/page.tsx
+++ src/app/admin/chat/[userId]/page.tsx
@@ -5,13 +5,13 @@ import UserProfile from "@/components/layout/user-profile";
 
 export default function AdminChatPage() {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <AppHeader title="Admin Chat">
         <UserProfile />
       </AppHeader>
-      <main className="flex-1 p-1 sm:p-2">
+      <main className="flex-1 p-1 sm:p-2 min-h-0">
         <AdminChatClient />
       </main>
-    </>
+    </div>
   );
 }
